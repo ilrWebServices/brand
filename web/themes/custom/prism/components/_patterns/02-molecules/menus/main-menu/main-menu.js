65075c178cfebf7a475cfbf14c9ee72528906816
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-06-28 14:48:06 -0400

    Fixing mobile nav and updating nav styles

--- web/themes/custom/prism/components/_patterns/02-molecules/menus/main-menu/main-menu.js
+++ web/themes/custom/prism/components/_patterns/02-molecules/menus/main-menu/main-menu.js
@@ -4,13 +4,7 @@
  *
  */
 
-// JavaScript should be made compatible with libraries other than jQuery by
-// wrapping it with an "anonymous closure". See:
-// - https://drupal.org/node/1446420
-// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
-
-
-(function ($, Drupal) { // UNCOMMENT IF DRUPAL.
+(function ($, Drupal) {
 
   Drupal.behaviors.mainMenu = {
     attach: function (context) {
@@ -32,29 +26,46 @@
               tl.pause();//$(this).addClass('hovered');
             }
             if (!expanded) {
-              toggle_expand.addClass('expanded');
-              toggle_expand.addClass('animating');
-              menu.addClass('main-nav--open');
-              expanded = true;
-              if (items.length) {
-                tl.restart();
-              } else {
-                items = $('.main-menu__item');
-                $(items).each(function(index){
-                  tl.from(this, timing, {top:"-=30px", autoAlpha: 0, ease: Power3.easeOut}, '-='+offset);
-                });
-              }
+              expandMenu();
             }
           },
           function(){},
         );
 
+        // Hide menu after they leave hover
         $(menu).hover(
           function(){},
           function() {
-            tl.reverse();
+            hideMenu();
           }
         );
+
+        $(toggle_expand).click(function(){
+          if (!expanded) {
+            expandMenu();
+          } else {
+            hideMenu();
+          }
+        });
+      }
+
+      function expandMenu() {
+        toggle_expand.addClass('expanded');
+        toggle_expand.addClass('animating');
+        menu.addClass('main-nav--open');
+        expanded = true;
+        if (items.length) {
+          tl.restart();
+        } else {
+          items = $('.main-menu__item');
+          $(items).each(function(index){
+            tl.from(this, timing, {top:"-=30px", autoAlpha: 0, ease: Power3.easeOut}, '-='+offset);
+          });
+        }
+      }
+
+      function hideMenu() {
+        tl.reverse();
       }
 
       function resetMenu() {
